65d053cdf3d91625e770b5b31c25050a20f1baf6
moey920 <56992106+moey920@users.noreply.github.com>
2022-03-17 21:51:38 +0900

    Update 영화감독 숌.py
    for문은 range를 확실히 정하지 못해 정답의 범위가 넘어갈 가능성이 있습니다.
    while 문으로 대체합니다.

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -3,19 +3,20 @@ def num_of_six():
     1의 자리수부터 검사해서 최소한의 n의 자리수에 6이 3개 이상 포함되어 있으면 리스트에 쌓는다.
     input:
         N: 결과의 N번째 원소
-        
+
     return:
         연속된 666이 포함된 수 중 N번째로 작은 수
     """
     N = int(input())
     result = []
-    for i in range(666, 999999999):
+    i = 0
+
+    while len(result) != N:
         if list(str(i)).count('6') >= 3:
             if '666' in str(i):
                 result.append(i)
-                if len(result) == N:
-                    break
-    print(result[N-1])
+        i += 1
+    print(result[N - 1])
 
 
 if __name__ == "__main__":
